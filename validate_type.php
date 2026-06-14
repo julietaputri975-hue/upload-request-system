2492287cfcb2af8508abf166c20a6ba606a9a8af
julietaputri975-hue <julietaputri975@gmail.com>
2026-06-14 16:55:21 +0700

    Delete validate_type.php

--- validate_type.php
+++
@@ -1,7 +0,0 @@
-<?php
-function validateType($file) {
-    $allowed = ["pdf", "doc", "docx"];
-    $ext = strtolower(pathinfo($file["name"], PATHINFO_EXTENSION));
-    return in_array($ext, $allowed);
-}
-?>
