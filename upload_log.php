3a4b6aa4a8bc4e827a77435666f3351a7f710332
asrinisa52-ai <asrinisa52@gmail.com>
2026-06-14 09:12:06 +0700

    Update upload_log.php

--- upload_log.php
+++ upload_log.php
@@ -1,9 +1,14 @@
 <?php
 
-function saveUploadLog($fileName)
+function uploadLog($fileName)
 {
     $log = date("Y-m-d H:i:s") . " - " . $fileName . PHP_EOL;
-    file_put_contents("upload.log", $log, FILE_APPEND);
+
+    file_put_contents(
+        "upload_log.txt",
+        $log,
+        FILE_APPEND
+    );
 }
 
 ?>
